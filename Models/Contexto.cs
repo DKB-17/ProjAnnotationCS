b5a03a56ffd126f2f657f2e45381f58691cbc1ec
diego <136401880+DKB-17@users.noreply.github.com>
2024-09-01 23:10:19 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjAnnotation.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) {}
+
+        public DbSet<Curso> Cursos { get; set; }
+    }
+}
---
+++ Models/Curso.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjAnnotation.Models
+{
+    [Table("Cursos")]
+    public class Curso
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [Required(ErrorMessage ="Campo obrigatorio")]
+        [StringLength(50)]
+        [Display(Name ="Descrição:")]
+        public string descricao { get; set; }
+
+    }
+}
